3972b62475c24508b30cd64f08f25ee10660edbe
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-12 01:13:00 -0500

    First submission

--- src/TopWordFinderTopologyPartA.java
+++ src/TopWordFinderTopologyPartA.java
@@ -16,27 +16,14 @@ import backtype.storm.tuple.Values;
 public class TopWordFinderTopologyPartA {
 
   public static void main(String[] args) throws Exception {
-
     TopologyBuilder builder = new TopologyBuilder();
+
+    builder.setSpout("spout", new RandomSentenceSpout(), 5);
+    builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
+    builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
 
     Config config = new Config();
     config.setDebug(true);
-
-
-    /*
-    ----------------------TODO-----------------------
-    Task: wire up the topology
-
-    NOTE:make sure when connecting components together, using the functions setBolt(name,…) and setSpout(name,…),
-    you use the following names for each component:
-
-    RandomSentanceSpout -> "spout"
-    SplitSentenceBolt -> "split"
-    WordCountBolt -> "count"
-
-
-    ------------------------------------------------- */
-
 
     config.setMaxTaskParallelism(3);
 
