55ceee6a3e3dd697aec54cecc560d9786e0f1c1b
Tyler Fowler <tylerfowler.1337@gmail.com>
2015-09-13 11:49:45 -0500

    Removed normalizer removing whitespace

--- src/NormalizerBolt.java
+++ src/NormalizerBolt.java
@@ -30,7 +30,7 @@ public class NormalizerBolt extends BaseBasicBolt {
     ------------------------------------------------- */
     String word = tuple.getString(0);
 
-    if (word.trim().isEmpty() || this.commonWords.contains(word.toLowerCase())) {
+    if (commonWords.contains(word.toLowerCase())) {
       return;
     } else {
       collector.emit(new Values(word.toLowerCase()));
